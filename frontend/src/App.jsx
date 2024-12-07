1cb2f14cd670c2bc544ce4165ed73f7a37d41c73
lavahang <llavahang@gmail.com>
2024-12-07 22:21:52 +0545

    created react router for home signup and login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
+import { Home } from "./pages/home"
+import { Login } from "./pages/login"
 import Signup from "./pages/signup"
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
 
   return (
     <>
-    <div>
-    <Signup/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/register" element={<Signup/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+      </Routes>   
+    </BrowserRouter>
+
     </>
   )
 }
