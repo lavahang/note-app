60fc5b4cc37156dfdc8478c0cf50145873efb5eb
lavahang <llavahang@gmail.com>
2025-01-01 21:15:51 +0545

    change made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Login from "./pages/login.jsx"
 import Signup from "./pages/signup"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import {ToastContainer} from 'react-toastify'
-// import 'react-toastify/dist/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
 
