f98bc6c9383b12487183eaf6e5904e1deb238bb1
Lavahang Phago <135295990+lavahang@users.noreply.github.com>
2025-01-29 21:40:37 +0545

    Update Note.js

--- server/models/Note.js
+++ server/models/Note.js
@@ -6,5 +6,11 @@ const NoteSchema = new mongoose.Schema({
     userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
 });
 
+// const NoteSchema = new mongoose.Schema({
+//     heading: { type: String, required: true},
+//     desc: {type: String, required: true},
+//     id: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+// });
+
 const Note = mongoose.model('Note', NoteSchema); //database name 'Note' le dincha
 export default Note
